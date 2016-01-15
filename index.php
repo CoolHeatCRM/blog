7a81973d7001075d05e448defbb0acde176487b3
CH-Dev <dev@coolheatcomfort.ca>
2016-01-15 15:32:43 -0500

    Initial Setup

---
+++ index.php
@@ -0,0 +1,201 @@
+<html>
+<head>
+<title> Hot Head</title>
+<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="shortcut icon" href="/icon.ico" />
+</head>
+<body>
+<!-- Creates the Header section -->
+<div class='header'>
+	<div class='LeftIcon'>
+	<img src="media/lefthead.png">
+	</div>
+	<div class='headtitle'>
+	</div>
+	<div class='RightIcon'>
+	<img src="media/righthead.png">
+	</div>
+	<div class='titletext'>
+	<p class='titletext'><u><b>Hot Head<br>
+	My thoughts on heating your home.</b></u></p> 
+	</div>
+	<div class='twitter'>
+		<a href="https://twitter.com/coolheatcomfortca" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @coolheatcomfortca</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	</div>
+	
+	<div class='facebook'><a href='https://www.facebook.com/CoolheatComfortSystems/'><img src='media/facebook.jpg' class='facebookimg'></img></a></div>
+	
+	<div class='linebreak'>
+	<hr>
+	</div>
+</div><!-- End of the creation of the header section -->
+<div class='sitebody'>
+	<div class='authorbox'>
+	<p class='authorname'>Who Am I?</p><hr class='authordivider'>
+	<img class='author' alt="Emily" src="media/emily.jpg">
+	<p class='authortext'>My name is Emily Mackenzie and I am the manager of media marketing at CoolHeat Comfort Systems. Together we are here to inform you on the different benefits of switching to a gas heating furnace.</p>
+	</div>
+
+<!-- This is the Article Template, just copy and paste from here till the next comment says it ends the Tempalte to create a new one! -->
+<!-- There are also comments marking special fields that need to be filled out for the particular article! -->
+	<div class='Article'>
+		<div class='articlebox'>
+			<div class='bufferbox'>
+			<h1>Tis the season for bad air quality!</h1><!-- Fill the H1 tag in with the Title of the article -->
+			<h2>8-Dec-2015   |   Emily Mackenzie</h2><!-- Fill the H2 tag in with the Date of the article -->
+			<img src='media/blueAC.jpg' class='articlepic'><!-- replace with an appropriate image for the article, These should be located in /media -->
+			
+			<p class='articletext'><!-- Fill in the article content here, stick under around 500 charachters or so, as the tempalte breaks after that -->
+				With winter rolling in, we all tend to retreat into our homes for long periods of time to avoid the frigid cold weather.
+				 With the amount of time we are spending indoors, it's surprising how overlooked the importance of air quality truly is.
+				  Air is our most basic necess...</p>
+			<div class='articlelinkbox'>
+				<a class='articleLink'>Read More</a><!-- please fill this in with a link to your full article! -->
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class='buffer'>
+	<!--  END OF TEMPLATE, this is the end of the template to copy/paste for new articles! -->
+	
+	
+	</div>
+	<div class='Article'>
+		<div class='articlebox'>
+			<div class='bufferbox'>
+			<h1>Tips to staying warm this winter.</h1>
+			<h2>30-Nov-2015   |   Emily Mackenzie</h2>
+			<img src='media/sloth.jpg' class='articlepic'>
+			
+			<p class='articletext'>
+				
+
+When winter rolls in, many of us all have the same dreadful thoughts; how cold it's going to be, and how light our wallets get with Holiday spending. So, we did some research and found some of the best energy saving tips to ensure you're staying warm and saving that ca...</p>
+			<div class='articlelinkbox'>
+				<a class='articleLink'>Read More</a>
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class='buffer'>
+	</div>
+	<div class='Article'>
+		<div class='articlebox'>
+			<div class='bufferbox'>
+			<h1>Heating with Propane & Propane Accessories.</h1>
+			<h2>21-Nov-2015   |   Emily Mackenzie</h2>
+			<img src='media/propane.png' class='articlepic'>
+			
+			<p class='articletext'>
+				
+Many people do not realize that you can efficienctly heat your home with the same gas we use in our barbecues; propane. Heating with propane will not only lower your utility costs, but will also make your home more comfortable and reduce your impact on the environment....</p>
+			<div class='articlelinkbox'>
+				<a class='articleLink'>Read More</a>
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class='buffer'>
+	</div>
+	<div class='Article'>
+		<div class='articlebox'>
+			<div class='bufferbox'>
+			<h1>Oil to Gas Conversion FAQ</h1>
+			<h2>13-Nov-2015   |   Emily Mackenzie</h2>
+			<img src='media/shinyfurnace.jpg' class='articlepic'>
+			
+			<p class='articletext'>
+				<b><u>What are some of the benefits of natural gas heating?</u></b><br><br>
+
+One of the most important benefits of natural gas heating is that it is generally more economical than oil based heating. Heating with natural gas is approximately half the price of heating with oil, so for example...</p>
+			<div class='articlelinkbox'>
+				<a class='articleLink'>Read More</a>
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class='buffer'>
+	</div>
+	<div class='Article'>
+		<div class='articlebox'>
+			<div class='bufferbox'>
+			<h1>Things to check before calling a furnace repairman.</h1>
+			<h2>5-Nov-2015   |   Emily Mackenzie</h2>
+			<img src='media/rayray.jpg' class='articlepic'>
+			
+			<p class='articletext'>
+				<b><u>What are some of the benefits of natural gas heating?</u></b><br><br>
+Disclaimer: The following article is a guideline to diagnosing minor issues with your gas furnace. These are simply tips and suggestions for running minimum diagnostic problems before calling your heating repairman. We here at Hot Head and CoolHeat Comfort Systems DO N...
+</p>
+			<div class='articlelinkbox'>
+				<a class='articleLink'>Read More</a>
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class='buffer'>
+	</div>
+	<div class='Article'>
+		<div class='articlebox'>
+			<div class='bufferbox'>
+			<h1>Three things heating companies don't want you to know</h1>
+			<h2>3-Nov-2015   |   Emily Mackenzie</h2>
+			<img src='media/shadyhand.jpg' class='articlepic'>
+			
+			<p class='articletext'>
+
+
+
+<b>1. Warranty's can be easily void.</b><br><br>
+
+Alot of furnace installers will provide promises of a worry free warranty that almost always sounds too good to be true. Many warranty's come in terms of 5-10 years, and while the companies can make them sound hassle free, there is...</p>
+			<div class='articlelinkbox'>
+				<a class='articleLink'>Read More</a>
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class='buffer'>
+	</div>
+	<div class='Article'>
+		<div class='articlebox'>
+			<div class='bufferbox'>
+			<h1>Top five reasons to heat with gas</h1>
+			<h2>13-Nov-2015   |   Emily Mackenzie</h2>
+			<img src='media/rheeem.jpg' class='articlepic'>
+			
+			<p class='articletext'>
+<b>1. Savings!</b><br><br>
+
+By switching to natural gas heating, you have the ultimate potential to save about $1000.00 annually on your heating bill. It's no secret that oil is at an all time high and doesn't seem to ever go down, and while gas prices can rise it is nowhere near t...</p>
+			<div class='articlelinkbox'>
+				<a class='articleLink'>Read More</a>
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class='buffer'>
+	</div>
+	<div class='Article'>
+		<div class='articlebox'>
+			<div class='bufferbox'>
+			<h1>The right guys, the right price.</h1>
+			<h2>13-Nov-2015   |   Emily Mackenzie</h2>
+			<img src='media/notadam.png' class='articlepic'>
+			
+			<p class='articletext'>
+ Offering convienent and professional service, Coolheat Comfort System is there for you every step of the way. They will perform a free assesment of your current furnace equiptment and suggest what is truly best for YOU. Your comfort is their priority.
+
+</p>
+			<div class='articlelinkbox'>
+				<a href='rightguys.php' class='articleLink'>Read More</a>
+			</div>
+			</div>
+		</div>
+	</div>
+	
+	
+</div>
+</body>
+</html>
---
+++ rightguys.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title> Hot Head</title>
+<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="shortcut icon" href="/icon.ico" />
+</head>
+<body>
+<!-- Creates the Header section -->
+<div class='header'>
+	<div class='LeftIcon'>
+	<img src="media/lefthead.png">
+	</div>
+	<div class='headtitle'>
+	</div>
+	<div class='RightIcon'>
+	<img src="media/righthead.png">
+	</div>
+	<div class='titletext'>
+	<p class='titletext'><u><b>Hot Head<br>
+	My thoughts on heating your home.</b></u></p> 
+	</div>
+	<div class='twitter'>
+		<a href="https://twitter.com/coolheatcomfortca" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @coolheatcomfortca</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	</div>
+	
+	<div class='facebook'><a href='https://www.facebook.com/CoolheatComfortSystems/'><img src='media/facebook.jpg' class='facebookimg'></img></a></div>
+	
+	<div class='linebreak'>
+	<hr>
+	</div>
+</div><!-- End of the creation of the header section -->
+
+</body>
+</html>
